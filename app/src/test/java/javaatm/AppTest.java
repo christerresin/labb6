6427845eda0efeec7cd7be2939fba7d287547514
Christerresin <christer.resin@gmail.com>
2022-09-14 14:59:44 +0200

    add test for deposit method on Costumer class

--- app/src/test/java/javaatm/AppTest.java
+++ app/src/test/java/javaatm/AppTest.java
@@ -7,8 +7,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+
+    @Test
+    void checkCostumerDeposit() {
+        Costumer costumerUnderTest = new Costumer("Test test");
+        assertTrue(costumerUnderTest.depositMoney(500));
+        assertEquals(500, costumerUnderTest.getBalance());
     }
 }
